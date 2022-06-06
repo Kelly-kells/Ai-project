73cff8694c9b4a3d3a586231f57547867efc1cb1
unknown <kellskelechi@gmail.com>
2022-06-06 13:17:40 +0100

    navbar

--- src/Components/navbar/Navbar.js
+++ src/Components/navbar/Navbar.js
@@ -1,9 +1,26 @@
 import React from 'react'
 import './navbar.css'
+import {RiMenu3Line, RiCloseLine} from 'react-icons/ri'
+import{IoLogoVk} from 'react-icons/io'
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div className='gpt3__navbar'>
+      <div className="gpt3__navbar-links">
+        <div className="gpt3__navbar-links-logo">
+          <IoLogoVk color="white" fontSize="3rem"/>
+        </div>
+        <div className="gpt3__navbar-links-container">
+          <p><a href="#home">Home</a></p>
+          <p><a href="#wgpt3">About</a></p>
+          <p><a href="#possibillity">Open Ai</a></p>
+          <p><a href="#features">Case studies</a></p>
+          <p><a href="#blog">Library</a></p>
+        </div>
+
+      </div>
+
+    </div>
   )
 }
 
