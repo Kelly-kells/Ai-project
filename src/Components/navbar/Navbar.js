effd59c7466ae9e3b0b5a9b3ef0316c747736de3
unknown <kellskelechi@gmail.com>
2022-06-07 17:57:37 +0100

    navbar responsive

--- src/Components/navbar/Navbar.js
+++ src/Components/navbar/Navbar.js
@@ -5,20 +5,9 @@ import { RiMenu3Line, RiCloseLine } from "react-icons/ri";
 import { IoLogoVk } from "react-icons/io";
 
 
-
-const Navbar = () => {
-
-
-  
-  const[toggleMenu, setTogleMenu]=useState(false)
-  return (
-    <div className="gpt3__navbar">
-      <div className="gpt3__navbar-links">
-        <div className="gpt3__navbar-links-logo">
-          <IoLogoVk color="white" fontSize="3rem" />
-        </div>
-        <div className="gpt3__navbar-links-container">
-          <p>
+const Menu = () => (
+  <>
+  <p>
             <a href="#home">Home</a> </p>
           <p>
             <a href="#wgpt3">About</a></p>
@@ -28,6 +17,22 @@ const Navbar = () => {
             <a href="#features">Case studies</a> </p>
           <p> <a href="#blog">Library</a>
           </p>
+  </>
+)
+
+
+
+const Navbar = () => {
+
+  const[toggleMenu, setTogleMenu]=useState(false)
+  return (
+    <div className="gpt3__navbar">
+      <div className="gpt3__navbar-links">
+        <div className="gpt3__navbar-links-logo">
+          <IoLogoVk color="white" fontSize="3rem" />
+        </div>
+        <div className="gpt3__navbar-links-container">
+          <Menu />
         </div>
       </div>
       <div className="gpt3__navbar-sign">
@@ -40,16 +45,12 @@ const Navbar = () => {
          {toggleMenu &&(
            <div className="gpt3__navbar-menu-container scale-up-center">
              <div className="gpt3__navbar-menu-container-links">
-             <p>
-            <a href="#home">Home</a> </p>
-          <p>
-            <a href="#wgpt3">About</a></p>
-          <p>
-            <a href="#possibillity">Open Ai</a> </p>
-          <p>
-            <a href="#features">Case studies</a> </p>
-          <p> <a href="#blog">Library</a>
-          </p>
+             <Menu />
+
+          <div className="gpt3__navbar-menu-container-links-sign">
+        <p>Sign in</p>
+        <button type="button">Sign up</button>
+      </div>
              </div>
            </div>
          )}
