5f550d67e2d4b7a2faefafe65005d15322a3bbe2
unknown <kellskelechi@gmail.com>
2022-06-14 20:44:48 +0100

    brands-section

--- src/Components/brand/Brand.js
+++ src/Components/brand/Brand.js
@@ -1,9 +1,27 @@
 import React from 'react'
 import './brand.css'
+import {FcGoogle} from "react-icons/fc"
+import {SiSlack} from "react-icons/si"
+import {TbBrandAirbnb} from "react-icons/tb"
+import {ImTwitch} from "react-icons/im"
+
 
 const Brand = () => {
   return (
-    <div>Brand</div>
+    <div className='gpt3__brands'>
+      <div className="gpt3__brands-contents">
+        
+      <FcGoogle color="white" fontSize="4rem" />
+      <SiSlack color="gray" fontSize="4rem"/>
+      < TbBrandAirbnb color="gray" fontSize="4rem"/>
+      <ImTwitch color="gray" fontSize="4rem"/>
+
+      </div>
+
+
+
+      
+    </div>
   )
 }
 
