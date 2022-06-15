c11cb18c9489098d85628cc03a88f363769d72fc
unknown <kellskelechi@gmail.com>
2022-06-15 22:15:05 +0100

    gpt3-sectiom

--- src/Components/brand/Brand.js
+++ src/Components/brand/Brand.js
@@ -3,18 +3,23 @@ import './brand.css'
 import {FcGoogle} from "react-icons/fc"
 import {SiSlack} from "react-icons/si"
 import {TbBrandAirbnb} from "react-icons/tb"
-import {ImTwitch} from "react-icons/im"
+import {FaShopify} from "react-icons/fa"
+import {GrDropbox} from "react-icons/gr"
+
+
 
 
 const Brand = () => {
   return (
-    <div className='gpt3__brands'>
+    <div className='gpt3__brands section__padding'>
       <div className="gpt3__brands-contents">
         
-      <FcGoogle color="white" fontSize="4rem" />
-      <SiSlack color="gray" fontSize="4rem"/>
-      < TbBrandAirbnb color="gray" fontSize="4rem"/>
-      <ImTwitch color="gray" fontSize="4rem"/>
+      <FcGoogle color="white" fontSize="3rem"  />
+      <SiSlack color="gray" fontSize="3rem"/>
+      < TbBrandAirbnb color="gray" fontSize="3rem"/>
+      <FaShopify color="gray" fontSize="3rem"/>
+      <GrDropbox color="gray" fontSize="3rem"/>
+
 
       </div>
 
