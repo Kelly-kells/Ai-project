30c963126bd5d072f2b8aacd43803a93eb657f99
unknown <kellskelechi@gmail.com>
2022-06-16 16:38:56 +0100

    whatGpt3

--- src/Containers/whatGPT3/WhatGPT3.js
+++ src/Containers/whatGPT3/WhatGPT3.js
@@ -1,11 +1,25 @@
 import React from 'react'
 import './whatGPT3.css'
+import { Feature } from '../../Components'
 
 const WhatGPT3 = () => {
   return (
-    <div className='gp3__whatGPT3 section-margin'>
-      
-
+    <div className='gp3__whatGPT3 section-margin' id>
+        
+    <div className="gpt3__whatGPT3-features">
+      <Feature/>
+    </div>
+    <div className="gpt3__whatGPT3-heading">
+      <h1 className='gradient-text'>
+        The Possibilities, Are Beyond Your Imagination
+      </h1>
+      <p>explore the library</p>
+    </div>
+    <div className="gpt3__whatGpt3-container">
+    <Feature/>
+    <Feature/>
+    <Feature/>
+    </div>
     </div>
   )
 }
