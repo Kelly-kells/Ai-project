ed723d1a04e21f28a53e41cf8d3f675c923558bd
unknown <kellskelechi@gmail.com>
2022-06-17 23:52:29 +0100

    feature

--- src/App.js
+++ src/App.js
@@ -1,28 +1,32 @@
+import React from "react";
+import { Brand, Cta, Navbar } from "./Components";
+import {
+  Blog,
+  Features,
+  Header,
+  Possibility,
+  WhatGPT3,
+  Footer,
+} from "./Containers";
 
-import React from 'react'
-import {Brand,Cta,Navbar} from'./Components'
-import{Blog,Features,Header,Possibility,WhatGPT3,Footer} from './Containers'
-
-import './index.css'
-
+import "./index.css";
 
 const App = () => {
   return (
-    <div  className='App'>
- <div className="gradient-bg">
-   <Navbar/>
-   <Header/>
- </div> 
-  <Brand/>
-  <WhatGPT3/>
-  <Features/>
-  <Possibility/>
-  <Cta/>
-  <Blog/>
-  <Footer/>
-     
+    <div className="App">
+      <div className="gradient-bg">
+        <Navbar />
+        <Header />
+      </div>
+      <Brand />
+      <WhatGPT3 />
+      <Features />
+      <Possibility />
+      <Cta />
+      <Blog />
+      <Footer />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- src/Containers/whatGPT3/WhatGPT3.js
+++ src/Containers/whatGPT3/WhatGPT3.js
@@ -1,27 +1,26 @@
-import React from 'react'
-import './whatGPT3.css'
-import { Feature } from '../../Components'
+import React from "react";
+import "./whatGPT3.css";
+import { Feature } from "../../Components";
 
 const WhatGPT3 = () => {
   return (
-    <div className='gpt3__whatGPT3 section-margin' id>
-        
-    <div className="gpt3__whatGPT3-feature">
-      <Feature/>
+    <div className="gpt3__whatGPT3 section-margin" id>
+      <div className="gpt3__whatGPT3-feature">
+        <Feature title={"What is GPT-3"} text={"We so opinion friends me message as delight. Whole front do of plate heard oh ought. His defective nor convinced residence own. Connection has put impossible own apartments boisterous. At jointure ladyship an insisted so humanity he. Friendly bachelor entrance to on by."} />
+      </div>
+      <div className="gpt3__whatGPT3-heading">
+        <h1 className="gradient-text">
+          The Possibilities Are Beyond Your Imagination.
+        </h1>
+        <p>explore the library</p>
+      </div>
+      <div className="gpt3__whatGpt3-container">
+        <Feature  />
+        <Feature />
+        <Feature />
+      </div>
     </div>
-    <div className="gpt3__whatGPT3-heading">
-      <h1 className='gradient-text'>
-        The Possibilities Are Beyond Your Imagination.
-      </h1>
-      <p>explore the library</p>
-    </div>
-    <div className="gpt3__whatGpt3-container">
-    <Feature/>
-    <Feature/>
-    <Feature/>
-    </div>
-    </div>
-  )
-}
+  );
+};
 
 export default WhatGPT3;
