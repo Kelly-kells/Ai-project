6fd48aeb1d338bbc5f0c0e2c4d3b75d9722a34c2
unknown <kellskelechi@gmail.com>
2022-06-25 11:58:36 +0100

    whatgpt3-end

--- src/Containers/whatGPT3/WhatGPT3.js
+++ src/Containers/whatGPT3/WhatGPT3.js
@@ -1,5 +1,5 @@
 import React from "react";
-import Feature from "../../components/feature/Feature";
+import Feature from "../../Components/feature/Feature";
 import "./whatGPT3.css";
 
 const WhatGPT3 = () => (
