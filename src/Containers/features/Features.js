0b257531dce4809c0a2fcaa83b25ba41349e1b13
unknown <kellskelechi@gmail.com>
2022-06-25 13:30:32 +0100

    features

--- src/Containers/features/Features.js
+++ src/Containers/features/Features.js
@@ -3,7 +3,17 @@ import './features.css'
 
 const Features = () => {
   return (
-    <div>Features</div>
+    <div className='gpt3__features section__padding'>
+      <div className="gpt3__features-heading">
+   <h1 className="gradient-text">
+   Let’s Build Something
+amazing with GPT-3
+OpenAI
+   </h1>
+   <p>Request Early Access to Get Started</p>
+      </div>
+
+    </div>
   )
 }
 
