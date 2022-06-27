6c6232fd88d19d4463c9a4fd4eb42704ec9d3818
unknown <kellskelechi@gmail.com>
2022-06-27 23:09:40 +0100

    features

--- src/Containers/features/Features.js
+++ src/Containers/features/Features.js
@@ -1,46 +1,45 @@
-import React from 'react'
-import './features.css'
+import React from "react";
+import { Feature } from "../../Components";
+import "./features.css";
 
-const featuresData=[
-
+const featuresData = [
   {
-    title:"Improving end distrusts instantly", 
-  text:"From they fine john he give of rich he. They age and draw mrs like. Improving end distrusts may instantly was household applauded."
-},
-{
-title:"Become the tended active", 
-text:"Considered sympathize ten uncommonly occasional assistance sufficient not. Letter of on become he tended active enable to."
-},
-
-{
-  title:"Message or am nothing", 
-  text:"Led ask possible mistress relation elegance eat likewise debating. By message or am nothing amongst chiefly address."
+    title: "Improving end distrusts instantly",
+    text: "From they fine john he give of rich he. They age and draw mrs like. Improving end distrusts may instantly was household applauded.",
+  },
+  {
+    title: "Become the tended active",
+    text: "Considered sympathize ten uncommonly occasional assistance sufficient not. Letter of on become he tended active enable to.",
   },
 
   {
-    title:"Really boy law county", 
-    text:"Led ask possible mistress relation elegance eat likewise debating. By message or am nothing amongst chiefly address."
-    },
-  
-]
+    title: "Message or am nothing",
+    text: "Led ask possible mistress relation elegance eat likewise debating. By message or am nothing amongst chiefly address.",
+  },
+
+  {
+    title: "Really boy law county",
+    text: "Led ask possible mistress relation elegance eat likewise debating. By message or am nothing amongst chiefly address.",
+  },
+];
 
 const Features = () => {
   return (
-    <div className='gpt3__features section__padding'>
+    <div className="gpt3__features section__padding">
       <div className="gpt3__features-heading">
-   <h1 className="gradient-text">
-   Let’s Build Something
-amazing with GPT-3
-OpenAI
-   </h1>
-   <p>Request Early Access to Get Started</p>
-   <div className="gpt3__feature-container">
-
-   </div>
+        <h1 className="gradient-text">
+          Let’s Build Something amazing with GPT-3 OpenAI
+        </h1>
+        <p>Request Early Access to Get Started</p>
+        <div className="gpt3__feature-container">
+        {featuresData.map((item, index)=>(
+          <Feature title={item.title} text={item.text} key={item.title + index}/>
+        ))}
+        </div>
+       
       </div>
-
     </div>
-  )
-}
+  );
+};
 
 export default Features;
