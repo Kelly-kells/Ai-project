df60378954ddf17bd1e0604a22f4507eedd00f6e
unknown <kellskelechi@gmail.com>
2022-06-29 23:41:24 +0100

    possibility-section

--- src/Containers/possibility/Possibility.js
+++ src/Containers/possibility/Possibility.js
@@ -1,10 +1,17 @@
-import React from 'react'
-import './possibility.css'
+import React from "react";
+import "./possibility.css";
+import possiImage from "../../assets/Images/possiImage.png";
 
 const Possibility = () => {
   return (
-    <div>Possibility</div>
-  )
-}
+    <div className="gpt__possibility section__padding" id="possibility">
+      <div className="possibility-image">
+        <img src={possiImage} alt="" />
+      </div>
+
+      <div className="gpt3__possibility-content"></div>
+    </div>
+  );
+};
 
 export default Possibility;
