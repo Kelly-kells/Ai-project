3bbe7de3a08e7b167fe3d0ac7fdf35bd28df06fb
unknown <kellskelechi@gmail.com>
2022-06-30 15:27:12 +0100

    flex

--- src/Containers/possibility/Possibility.js
+++ src/Containers/possibility/Possibility.js
@@ -9,7 +9,17 @@ const Possibility = () => {
         <img src={possiImage} alt="" />
       </div>
 
-      <div className="gpt3__possibility-content"></div>
+      <div className="gpt3__possibility-content">
+        <h1 className="gradient-text">
+          The Possibilities Are Beyond Your Imagination
+        </h1>
+        <p>
+          Yet bed any for travelling assistance indulgence unpleasing. Not
+          thoughts all exercise blessing. Indulgence way everything joy
+          alteration boisterous the attachment. Party we years to order allow
+          asked of.
+        </p>
+      </div>
     </div>
   );
 };
