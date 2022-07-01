8b0914fdd1e62816a0efcb95a89545fb92b1e743
unknown <kellskelechi@gmail.com>
2022-07-01 14:33:59 +0100

    CTA

--- src/Components/cta/Cta.js
+++ src/Components/cta/Cta.js
@@ -3,7 +3,17 @@ import './cta.css'
 
 const Cta = () => {
   return (
-    <div>Cta</div>
+    <div className='gpt3__cta section-padding'>
+    <div className="gpt3__cta-content">
+      <h5>Request Early Access to Get Started.</h5>
+      <h1>Register today & start exploring the endless possiblities.</h1>
+      
+    </div>
+
+    <div>
+      <button type='button'>Get started</button>
+      </div>
+    </div>
   )
 }
 
