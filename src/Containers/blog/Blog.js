dc66d6a51bb6d08575d60626f47c16e1c725390f
unknown <kellskelechi@gmail.com>
2022-07-03 14:15:10 +0100

    article

--- src/Containers/blog/Blog.js
+++ src/Containers/blog/Blog.js
@@ -1,9 +1,27 @@
 import React from 'react'
 import './blog.css'
+import {Article} from '../../Components'
 
 const Blog = () => {
   return (
-    <div>Blog</div>
+    <div className='gpt3__blog section-padding'>
+      <div className="gpt3__blog-heading">
+        <h1 className='gradient-text'>A lot is happening, 
+We are blogging about it.</h1>
+      </div>
+      <div className="gpt3__blog-container">
+       <div className="gbpt3__blog-container__groupA">
+    <Article/>
+       </div>
+       <div className="gbpt3__blog-container__groupB">
+       <Article/>
+       <Article/>
+       <Article/>
+       <Article/>
+        </div>
+        
+      </div>
+    </div>
   )
 }
 
