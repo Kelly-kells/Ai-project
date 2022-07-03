31367e3efb80c7e59f8d0f43a6bd2e29d7833c6e
unknown <kellskelechi@gmail.com>
2022-07-04 00:28:43 +0100

    blog-article

--- src/Components/article/Article.js
+++ src/Components/article/Article.js
@@ -1,15 +1,15 @@
-import React from 'react'
-import './article.css'
+import React from "react";
+import "./article.css";
 
-const Article = ({imgUrl}) => {
+const Article = ({ imgUrl, date, title }) => {
   return (
-    <div className=' gpt3__blog-container-article'>
+    <div className=" gpt3__blog-container-article">
       <div className="gpt3__blog-container-article-image">
         <img src={imgUrl} alt="" />
       </div>
-
+      <div className="gpt3__blog-article-content"></div>
     </div>
-  )
-}
+  );
+};
 
 export default Article;
--- src/Containers/blog/Blog.js
+++ src/Containers/blog/Blog.js
@@ -1,28 +1,44 @@
-import React from 'react'
-import './blog.css'
-import {Article} from '../../Components'
-import { blog01, blog02, blog03, blog04, blog05 } from './import'
+import React from "react";
+import "./blog.css";
+import { Article } from "../../Components";
+import { blog01, blog02, blog03, blog04, blog05 } from "./import";
 const Blog = () => {
   return (
-    <div className='gpt3__blog section-padding'>
+    <div className="gpt3__blog section-padding">
       <div className="gpt3__blog-heading">
-        <h1 className='gradient-text'>A lot is happening, 
-We are blogging about it.</h1>
+        <h1 className="gradient-text">
+          A lot is happening, We are blogging about it.
+        </h1>
       </div>
       <div className="gpt3__blog-container">
-       <div className="gbpt3__blog-container__groupA">
-    < Article imgUrl={blog01}/>
-       </div>
-       <div className="gbpt3__blog-container__groupB">
-       < Article imgUrl={blog02}/>
-       < Article imgUrl={blog03}/>
-       < Article imgUrl={blog04}/>
-       < Article imgUrl={blog05}/>
+        <div className="gbpt3__blog-container__groupA">
+          <Article imgUrl={blog01} />
         </div>
-        
+        <div className="gbpt3__blog-container__groupB">
+          <Article
+            imgUrl={blog02}
+            date=" june 12 2022"
+            title="Let's figure it out together"
+          />
+          <Article
+            imgUrl={blog03}
+            date=" june 12 2022"
+            title="Let's figure it out together"
+          />
+          <Article
+            imgUrl={blog04}
+            date=" june 12 2022"
+            title="Let's figure it out together"
+          />
+          <Article
+            imgUrl={blog05}
+            date=" june 12 2022"
+            title="Let's figure it out together"
+          />
+        </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default Blog;
