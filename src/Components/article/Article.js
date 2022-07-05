4c59ff7b0d0191d9c1cbf73412b1b678ce5afd9f
unknown <kellskelechi@gmail.com>
2022-07-05 14:23:48 +0100

    blog-section

--- src/Components/article/Article.js
+++ src/Components/article/Article.js
@@ -7,7 +7,11 @@ const Article = ({ imgUrl, date, title }) => {
       <div className="gpt3__blog-container-article-image">
         <img src={imgUrl} alt="" />
       </div>
-      <div className="gpt3__blog-article-content"></div>
+      <div className="gpt3__blog-article-content">
+        <p>{date}</p>
+        <h2>{title}</h2>
+        <p>Read more..</p>
+      </div>
     </div>
   );
 };
