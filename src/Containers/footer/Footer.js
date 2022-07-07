4d29252834c21e41f7d0148ac020cfcb6e1f5a95
unknown <kellskelechi@gmail.com>
2022-07-07 20:42:50 +0100

    footer

--- src/Containers/footer/Footer.js
+++ src/Containers/footer/Footer.js
@@ -1,9 +1,22 @@
 import React from 'react'
 import './footer.css'
+import { IoLogoVk } from "react-icons/io";
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <div className='gpt3__footer section__padding'>
+     <div className="gpt3__footer-btn">
+       <p>Request Early access</p>
+     </div>
+
+     <div className="gpt3__footer-links">
+      <div className="gpt3__footer-links__logo">
+      <IoLogoVk color="white" fontSize="3rem" />
+      <p>Cretchwood 173 waf abigjan</p>
+      </div>
+     </div>
+
+    </div>
   )
 }
 
